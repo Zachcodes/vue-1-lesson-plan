a7cbdbcac74252f8bb419961fb9250f7e6447502
zach <zaspringer@gmail.com>
2018-12-11 11:46:00 -0700

    Finished lesson plan

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Step 10 (bring in time of day using computer property) && Step 11 (Show computed using greeting)-->
+    {{greeting}} {{timeOfDay}}
+    <!-- Step 5 -->
+    <Todo v-for="list in lists"
+    :key="list.id"
+    :id="list.id"
+    :listTitle="list.listTitle"
+    :list="list.items"
+    :addItemToList="addItemToList"
+    :deleteItem="deleteItem"/>
+    <!-- Step 3 -->
+    <!-- <div class="todo-main-container">Hello</div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todo
+  },
+  // Step 10 (Show off lifecycle methods with this.timeIntervalId)
+  mounted() {
+    this.timeIntervalId = setInterval(() => this.timeOfDay = new Date().toLocaleString(), 1000)
+  },
+  data() {
+    return {
+      // Step 5
+      lists: [
+        {
+          id: 1,
+          items: ['Get groceries', 'Registration', 'Finish book'],
+          listTitle: 'My Day'
+        },
+        {
+          id: 2,
+          items: ['Salmon', 'Salad', 'Dressing'],
+          listTitle: 'Shopping List'
+        },
+        {
+          id: 3,
+          items: ['Gift cards', 'Candles', 'Airpods'],
+          listTitle: 'Christmas shopping list'
+        }
+      ],
+      // Step 10
+      timeIntervalId: 0,
+      timeOfDay: new Date().toLocaleString(),
+      // Step 11 (add first name)
+      firstName: 'Zach'
+    }
+  },
+  methods: {
+    // Step 6
+    addItemToList(id, newItem) {
+      for(let i = 0; i < this.lists.length; i++) {
+        if(this.lists[i].id === id) this.lists[i].items.push(newItem)
+      }
+    },
+    // Step 8
+    deleteItem(id, itemIndex) {
+      for(let i = 0; i < this.lists.length; i++) {
+        if(this.lists[i].id === id) this.lists[i].items.splice(itemIndex, 1)
+      }
+    }
+  },
+  computed: {
+    // Step 11 (bring in computed properties)
+    greeting() {
+      return `Hello ${this.firstName}, the time and date are`
+    }
   }
 }
 </script>
@@ -24,5 +85,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="todo-main-container">
+        <!-- Step 2 -->
+        <div>{{listTitle}}</div>
+        <!-- Step 1 -->
+        <ul v-if="list.length">
+            <li v-for="(item, i) in list">
+                 <!-- Step 8 (add delete item) -->
+                {{item}} <button v-on:click="deleteItem(id, i)">Delete Item</button>
+            </li>
+        </ul>
+        <!-- Step 9 (add v-else and v-if) -->
+        <div v-else>No items currently</div>
+        <!-- Step 4 -->
+        <!-- New Item: <input v-model="newItem" v-on:keyup.enter="addItem"/> <button v-on:click="addItem">Add list item</button> -->
+        <!-- Step 7 (Alter this v-on:keyup and v-on:click to call a method passed from the parent) -->
+        New Item: <input v-model="newItem" v-on:keyup.enter="addItem"/> <button v-on:click="addItem">Add list item</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo',
+    // Step 2 
+    // props: ['listTitle'],
+    // Step 5
+    props: ['listTitle', 'list', 'id', 'addItemToList', 'deleteItem'],
+    data() {
+        return {
+            // Step 5
+            // list: ['Get groceries', 'Registration', 'Finish book'],
+            newItem: ''
+        }
+    },
+    methods: {
+        // Step 7 (update function)
+        addItem() {
+            if(this.newItem.length) {
+              this.addItemToList(this.id, this.newItem)
+              this.newItem = ''
+            } 
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* Step 3 */
+.todo-main-container {
+    width: 300px;
+    border: 1px solid black;
+    margin: 5px;
+}
+</style>
