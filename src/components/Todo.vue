cb80f548e987ab7e41c5ed38e2591631d2959bea
zach <zaspringer@gmail.com>
2018-12-12 09:34:48 -0700

    Added some optional things that can be done if we finish early today

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="todo-main-container">
+    <!-- <div class="todo-main-container"> -->
+        <!-- Optional step after completion -->
+    <div v-bind:class="[list.length ? hasItems : noItems, todoMainContainer]">
         <!-- Step 2 -->
         <div>{{listTitle}}</div>
         <!-- Step 1 -->
-        <ul v-if="list.length">
-            <li v-for="(item, i) in list">
+        <!-- <ul v-if="list.length">
+            <li v-for="(item, i) in list"> -->
                  <!-- Step 8 (add delete item) -->
-                {{item}} <button v-on:click="deleteItem(id, i)">Delete Item</button>
+                <!-- {{item}} <button v-on:click="deleteItem(id, i)">Delete Item</button>
             </li>
+        </ul> -->
+        <!-- Optional step here for if finish early -->
+        <ul v-if="list.length">
+            <transition-group name="list-transition">
+                <li v-for="(item, i) in list" v-bind:key="item">
+                    <!-- Step 8 (add delete item) -->
+                    {{item}} <button v-on:click="deleteItem(id, i)">Delete Item</button>
+                </li>
+            </transition-group>
         </ul>
         <!-- Step 9 (add v-else and v-if) -->
         <div v-else>No items currently</div>
@@ -20,7 +31,7 @@
 
 <script>
 export default {
-    name: 'todo',
+    name: 'Todo',
     // Step 2 
     // props: ['listTitle'],
     // Step 5
@@ -29,7 +40,10 @@ export default {
         return {
             // Step 5
             // list: ['Get groceries', 'Registration', 'Finish book'],
-            newItem: ''
+            newItem: '',
+            hasItems: 'has-items',
+            noItems: 'no-items',
+            todoMainContainer: 'todo-main-container'
         }
     },
     methods: {
@@ -51,5 +65,19 @@ export default {
     width: 300px;
     border: 1px solid black;
     margin: 5px;
+}
+.has-items {
+    box-shadow: 3px 3px 3px green;
+}
+.no-items {
+    box-shadow: 3px 3px 3px red;
+}
+/* Add if there is time at the end of the class */
+.list-transition-enter-active, .list-transition-leave-active {
+  transition: all 1s;
+}
+.list-transition-enter, .list-transition-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
